ac7dc60f0333ae7dba3503e23ee12dd1f7d5a75f
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-08-31 13:09:31 -0500

    ability to add more projects
    -asks user if they want to add a project
    -if they say no or write nothing it prints no new projects and chooses a project
    -else it adds the new project to the project_ideas list prints a statement and chooses a project idea

--- projects/choose_project.py
+++ projects/choose_project.py
@@ -2,5 +2,12 @@ from random import choice
 
 project_ideas = ['login system', 'blackjack', 'turtle game', 'expense tracker excel']
 
-print(choice(project_ideas))
+greeting = input('Would you like to add a project?\n')
+if greeting == '' or greeting == 'no': 
+    print('No new projects')
+else: 
+    project_ideas.append(greeting)
+    print(f'New project added: {greeting}')
+
+print('Choice: ', choice(project_ideas))
 
