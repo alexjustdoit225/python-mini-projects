7505438e566b9b408c5dacc7cbdae6cb2c541c04
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-08-31 17:15:36 -0500

    get number of turtles
    -created function to ask the user how many turtles are racing and return that as a number
    -text has to be a number(isdigit())
    -number has to be between 2 and 10

--- projects/turtle_race/turtle_race.py
+++ projects/turtle_race/turtle_race.py
@@ -9,6 +9,20 @@ def canvas_setup():
     canvas.screensize(WIDTH, HEIGHT)
 
 def get_number_of_turtles():
-    greeting = input('How many turtles are racing today? ')
-canvas_setup()
-time.sleep(5)
+    while True: 
+        greeting = input('How many turtles are racing today? (2 - 10)\n')
+        if greeting.isdigit(): 
+            num_of_turtles = int(greeting)
+        else: 
+            print("That's not a number. Try Again\n")
+            continue
+        
+        if 2 <= num_of_turtles <= 10: 
+            return num_of_turtles
+        else: 
+            print('Number should be between 2 and 10. Try Again\n')
+            continue
+#canvas_setup()
+#time.sleep(5)
+get_number_of_turtles()
+
