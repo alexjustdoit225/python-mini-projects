14ca58c5be4d78e290afe914c57b34ccc822a20d
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-09-02 18:44:08 -0500

    added collision detection
    -added another player with collision detection

--- projects/pygame_intro/pygame_intro.py
+++ projects/pygame_intro/pygame_intro.py
@@ -6,8 +6,17 @@ WIDTH, HEIGHT = 800, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('First Game')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('projects\\pygame_intro\\font\\Pixeltype.ttf', 50)
 
-sky = pygame.image.load('graphics/Sky.png')
+sky_surface = pygame.image.load('projects\pygame_intro\graphics\Sky.png').convert()
+ground_surface = pygame.image.load('projects\pygame_intro\graphics\ground.png').convert()
+text_surface = test_font.render("First Game", False, 'black')
+
+snail_surface = pygame.image.load('projects\pygame_intro\graphics\snail\snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright= (700, 300))
+
+player1_surf = pygame.image.load('projects\pygame_intro\graphics\Player\player_walk_1.png').convert_alpha()
+player1_rect = player1_surf.get_rect(midbottom= (80, 300))
 
 while True: 
     #checks for any player input
@@ -16,7 +25,24 @@ while True:
             pygame.quit()
             exit() #most secure way to exit pygame
     #draw all of our elements
-    screen.blit(test_surface, (200,100))
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300, 50))
+    snail_rect.left += 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    elif snail_rect.left >= 800: snail_rect.right = 0
+    screen.blit(snail_surface, snail_rect)
+    
+    player1_rect.left += 2
+    if player1_rect.right <= 0: player1_rect.left = 800
+    elif player1_rect.left >= 800: player1_rect.right = 0
+    screen.blit(player1_surf, player1_rect)
+    
+    #get x,y mouse position
+
+    #collision detection
+    if player1_rect.colliderect(snail_rect): 
+        print('collided')
     #updates everything
     pygame.display.update()
     clock.tick(60)
