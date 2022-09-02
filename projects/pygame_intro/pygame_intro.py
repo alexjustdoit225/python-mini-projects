bc0f47900955a0d5275beaf331514a31947f7dde
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-09-02 11:48:53 -0500

    Create pygame_intro.py
    Created a pygame program

---
+++ projects/pygame_intro/pygame_intro.py
@@ -0,0 +1,19 @@
+import pygame
+from sys import exit
+
+pygame.init()
+WIDTH, HEIGHT = 800, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('First Game')
+clock = pygame.time.Clock()
+
+while True: 
+    #checks for any player input
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            exit() #most secure way to exit pygame
+    #draw all of our elements
+    #updates everything
+    pygame.display.update()
+    clock.tick(60)
