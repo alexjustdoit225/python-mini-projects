725e73ed26f0fea88b3562e03babe650b30d57ab
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-09-06 19:02:33 -0500

    created & positioned player
    -created a player class
    -added to level_map
    -defined in level setup
    -drawn when the level is run

--- projects/platform_game/level.py
+++ projects/platform_game/level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 class Level(): 
     def __init__(self, level_map, surface):
@@ -11,19 +12,30 @@ class Level():
         
     def level_setup(self, map): 
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(map): 
             for col_index, cell in enumerate(row): 
-                print(f'({row_index}, {col_index}): {cell}')
+                x = col_index * tile_size
+                y = row_index * tile_size
+                
                 if cell == 'X': 
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
+                if cell == 'P': 
+                    player_1 = Player((x, y))
+                    self.player.add(player_1)
         
         
     def run(self):
+        
+        # Level Tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        
+        # Player
+        self.player.draw(self.display_surface)
+        # self.player.update()
         
         
                 
--- projects/platform_game/settings.py
+++ projects/platform_game/settings.py
@@ -7,7 +7,7 @@ level_map = [
 ' XXXX         XX         XX ',
 ' XXXX       XX              ',
 ' XX    X  XXXX    XX  XX    ',
-'       X  XXXX    XX  XXX   ',
+'     P X  XXXX    XX  XXX   ',
 '    XXXX  XXXXXX  XX  XXXX  ',
 'XXXXXXXX  XXXXXX  XX  XXXX  ']
 
